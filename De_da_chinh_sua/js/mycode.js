0686e81d316d839c48393733b2880401f4531f49
Vadata <dutaen1011@gmail.com>
2022-09-17 14:52:36 +0700

    Up load WED FA21 de2 (1)

---
+++ De_da_chinh_sua/js/mycode.js
@@ -0,0 +1,6 @@
+function customerBuy(){
+    var phone =prompt('Please enter your number phone(11): ');
+    if(phone.length !=0 && phone.length == 11){
+    alert('we messing for you, please enter OPT right here: ');
+    } alert('Number does not exist');
+}
